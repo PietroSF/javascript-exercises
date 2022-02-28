6fddaaabc2c88a05bd52a77f3e855cd0f475b4a9
PietroSF <pietrosgfoltran@gmail.com>
2022-02-28 17:51:00 -0300

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(rvString) {
+/*  take string
+    turn into array
+    reverse array
+    turn back into string */
 
+  const strArray = [...rvString];
+  const rvArray = strArray.reverse();
+  return reversedString = rvArray.join("");
 };
 
 // Do not edit below this line
