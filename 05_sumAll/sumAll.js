71526154350f83eed50b49d20072bd9730012571
PietroSF <pietrosgfoltran@gmail.com>
2022-03-01 03:13:03 -0300

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+/*  check whether a or b is smallest
+    smallest is start of index
+    c = smallest
+    while index < largest, ++
+    add index to c
+    when index = biggest, add index to c and return c */
+
+const sumAll = function(firstInt, secondInt) {
+
+  if (((typeof secondInt)!='number')||((typeof firstInt)!='number')) {
+    return 'ERROR';
+  };
+  
+  if ((firstInt < 0)||(secondInt < 0)) {
+    return 'ERROR';
+  };
+
+  if (firstInt < secondInt) {
+    smallest = firstInt;
+    largest = secondInt;
+  } else if (firstInt => secondInt) {
+    smallest = secondInt;
+    largest = firstInt;
+  } 
+    
+  let index = smallest;
+  let subtractFix = smallest;
+
+  while (index <= largest) {
+    smallest = smallest + index;
+    index++;
+  }
+  return smallest - subtractFix;
 
 };
 
